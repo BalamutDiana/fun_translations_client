f155f33de4174748fa35e010e3b5ca42e0b3144d
BalamutDiana <67107383+BalamutDiana@users.noreply.github.com>
2022-08-03 12:54:53 +0300

    update main.go
    placed logic in packages

--- main.go
+++ main.go
@@ -1,62 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"golang-ninja/httpclient/funtranslations"
 	"log"
-	"net/http"
-	"os"
 	"time"
 )
 
-type loggingRoundTripper struct {
-	logger io.Writer
-	next   http.RoundTripper
-}
-
-func (l loggingRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	fmt.Fprintf(l.logger, "[%s] %s %s\n", time.Now().Format(time.ANSIC), r.Method, r.URL)
-	return l.next.RoundTrip(r)
-}
-
 func main() {
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			fmt.Println(req.Response.Status)
-			fmt.Println("REDIRECT")
-			return nil
-		},
-		Transport: &loggingRoundTripper{
-			logger: os.Stdout,
-			next:   http.DefaultTransport,
-		},
-		Timeout: time.Second * 5,
-	}
-
-	values := map[string]string{"text": "Hello little bro"}
-	json_data, err := json.Marshal(values)
-
+	funtranslationsClient, err := funtranslations.NewClient(time.Second * 10)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err := client.Post("https://api.funtranslations.com/translate/pirate.json", "application/json",
-		bytes.NewBuffer(json_data))
+	trans, err := funtranslationsClient.GetTranslation()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.StatusCode)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(body))
+	fmt.Println(trans.Info())
 }
