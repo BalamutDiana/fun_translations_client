26b44da1a448658303ad2a0f18182e8689f35371
BalamutDiana <67107383+BalamutDiana@users.noreply.github.com>
2022-08-04 13:42:23 +0300

    update client.go
    add rate limit detector

--- funtranslations/client.go
+++ funtranslations/client.go
@@ -4,17 +4,29 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
 )
 
+type Languages struct {
+	Pirate string
+}
+
+func LanguagesList() *Languages {
+	return &Languages{
+		Pirate: "pirate",
+	}
+}
+
 type Client struct {
 	client *http.Client
 }
 
 func NewClient(timeout time.Duration) (*Client, error) {
+
 	if timeout == 0 {
 		return nil, errors.New("timeout can't be zero")
 	}
@@ -30,34 +42,69 @@ func NewClient(timeout time.Duration) (*Client, error) {
 	}, nil
 }
 
-func (c Client) GetTranslation() (ContentsData, error) {
-	values := map[string]string{"text": "Hello little bro"}
+func (c Client) GetTranslation(language, text string) (ResponseData, error) {
+	values := map[string]string{"text": text}
 	json_data, err := json.Marshal(values)
 
 	if err != nil {
 		return ContentsData{}, err
 	}
 
-	resp, err := c.client.Post("https://api.funtranslations.com/translate/pirate.json", "application/json",
-		bytes.NewBuffer(json_data))
+	url := fmt.Sprintf("https://api.funtranslations.com/translate/%s.json", language)
+	resp, err := c.client.Post(url, "application/json", bytes.NewBuffer(json_data))
 	if err != nil {
 		return ContentsData{}, err
 	}
-
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	available, err := checkAvailability(resp)
 	if err != nil {
 		return ContentsData{}, err
 	}
 
-	//fmt.Println(string(body))
-	var r assetsResponse
-	err = json.Unmarshal(body, &r)
-	if err != nil {
-		return ContentsData{}, err
+	if available {
+		var r assetsResponse
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return r.Contents, err
+		}
+
+		err = json.Unmarshal(body, &r)
+		if err != nil {
+			return r.Contents, err
+		}
+
+		return r.Contents, nil
+
+	} else {
+
+		var r assetsError
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return r.Contents, err
+		}
+
+		err = json.Unmarshal(body, &r)
+		if err != nil {
+			return r.Contents, err
+		}
+
+		return r.Contents, nil
 	}
 
-	return r.Contents, nil
-	//fmt.Println(r.Contents.Info())
+}
+
+func checkAvailability(resp *http.Response) (bool, error) {
+
+	switch resp.StatusCode {
+	case 200:
+		return true, nil
+	case 429:
+		return false, nil
+	default:
+		return false, errors.New("something went wrong, check the request or try again later")
+	}
+
 }
